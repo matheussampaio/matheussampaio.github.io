945c82857e69db1e90aefc9b7f056013a2c49775
Matheus Sampaio <matheus1401@gmail.com>
2015-02-06 02:01:14 +0000

    Modificação no titulo das paginas e adicionado alias no vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@
 apt-get update
 
 # Instalando Dependencias:
-apt-get install -y make build-essential nodejs ruby1.9.3
+apt-get install -y make build-essential nodejs ruby1.9.3 vim
 
 gem install bundler
+
+echo "alias blog='cd /vagrant; bundle exec jekyll serve -D --force_polling'" >> ~/.bash_aliases
