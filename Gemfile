de385e5d78db7ee243088f20db48e1ba3c1aed7f
Matheus Sampaio <matheus1401@gmail.com>
2015-09-21 14:32:11 -0500

    Add some plugins and url

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'jekyll-compose'
 gem 'github-pages'
+gem 'jekyll-gist'
 gem 'jemoji'
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,4 @@ apt-get install -y make build-essential nodejs ruby1.9.3 vim
 gem install bundler
 
 echo "alias blog='cd /vagrant; bundle exec jekyll serve -D --force_polling'" >> ~/.bash_aliases
+source "~/.bashrc"
